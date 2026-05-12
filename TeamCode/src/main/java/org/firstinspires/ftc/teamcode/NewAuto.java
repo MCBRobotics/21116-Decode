d2fdb1d407af26c0618e002c150ed9d74105860e
N Wright <MCBRoboticss@gmail.com>
2026-05-12 08:27:47 +0100

    Changed NewDriveSystem to a non-linear OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAuto.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.pedropathing.follower.Follower;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
-
 
 @Autonomous (name = "NewAuto", group = "Auto")
 public class NewAuto extends OpMode {
@@ -19,7 +20,11 @@ public class NewAuto extends OpMode {
     DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "rightbackdrive");
 
     /*DECLARE POSES */
-    Pose startingPose = new Pose(72, 72, Math.toRadians(180));
+    Pose startingPose = new Pose(72, 72, Math.toRadians(90));
+    Pose endingPose = new Pose(72, 60, Math.toRadians(270));
+
+    /*DECLARE PATHs */
+    Path firstPath = new Path(new BezierLine(startingPose, endingPose));
 
     Follower follower = Constants.createFollower(hardwareMap);
 
@@ -30,12 +35,11 @@ public class NewAuto extends OpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-
-
         follower.setStartingPose(startingPose);
     }
 
     @Override
     public void loop() {
+        follower.followPath(firstPath);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewDriveSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewDriveSystem.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -10,10 +10,16 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp (name = "NewDriveSystem", group = "TeleOp")
-public class NewDriveSystem extends LinearOpMode {
+public class NewDriveSystem extends OpMode {
+    
+    DcMotor leftFrontDrive = null;
+    DcMotor leftBackDrive = null;
+    DcMotor rightFrontDrive = null;
+    DcMotor rightBackDrive = null;
+    IMU imu = null;
 
     public double applyDeadzone(double joystick) {
-        double deadzone_boundary = 0.07;
+        double deadzone_boundary = 0.1;
         if (joystick < deadzone_boundary && joystick > -deadzone_boundary ) {
             return 0.0;
         } else {
@@ -22,18 +28,18 @@ public class NewDriveSystem extends LinearOpMode {
     }
 
     @Override
-    public void runOpMode() {
-        DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, "leftfrontdrive");
-        DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, "leftbackdrive");
-        DcMotor rightFrontDrive = hardwareMap.get(DcMotor.class, "rightfrontdrive");
-        DcMotor rightBackDrive = hardwareMap.get(DcMotor.class, "rightbackdrive");
+    public void init() {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "leftfrontdrive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "leftbackdrive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightfrontdrive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightbackdrive");
 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        IMU imu = hardwareMap.get(IMU.class, "imu");
+        imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
                         RevHubOrientationOnRobot.LogoFacingDirection.UP,
@@ -43,38 +49,38 @@ public class NewDriveSystem extends LinearOpMode {
 
         imu.initialize(parameters);
         imu.resetYaw();
-        waitForStart();
 
-        while (opModeIsActive()) {
+    }
+    
+    @Override
+    public void loop() {
+        double axial = applyDeadzone(-gamepad1.left_stick_y);
+        double lateral = applyDeadzone(gamepad1.left_stick_x);
+        double yaw = applyDeadzone(gamepad1.right_stick_x);
 
-            double axial = applyDeadzone(-gamepad1.left_stick_y);
-            double lateral = applyDeadzone(gamepad1.left_stick_x);
-            double yaw = applyDeadzone(gamepad1.right_stick_x);
+        axial = Math.signum(axial) * Math.pow(axial, 2);
+        lateral = Math.signum(lateral) * Math.pow(lateral, 2);
+        yaw = Math.signum(yaw) * Math.pow(yaw, 2);
 
-            axial = Math.signum(axial) * Math.pow(axial, 2);
-            lateral = Math.signum(lateral) * Math.pow(lateral, 2);
-            yaw = Math.signum(yaw) * Math.pow(yaw, 2);
+        YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
+        double heading = angles.getYaw(AngleUnit.RADIANS);
 
-            YawPitchRollAngles angles = imu.getRobotYawPitchRollAngles();
-            double heading = angles.getYaw(AngleUnit.RADIANS);
+        double rotX = lateral * Math.cos(-heading) - axial * Math.sin(-heading);
+        double rotY = lateral * Math.sin(-heading) + axial * Math.cos(-heading);
 
-            double rotX = lateral * Math.cos(-heading) - axial * Math.sin(-heading);
-            double rotY = lateral * Math.sin(-heading) + axial * Math.cos(-heading);
+        double leftfrontPower = rotY + rotX + yaw;
+        double rightfrontPower = rotY - rotX - yaw;
+        double leftbackPower = rotY - rotX + yaw;
+        double rightbackPower = rotY + rotX - yaw;
 
-            double leftfrontPower = rotY + rotX + yaw;
-            double rightfrontPower = rotY - rotX - yaw;
-            double leftbackPower = rotY - rotX + yaw;
-            double rightbackPower = rotY + rotX - yaw;
-
-            leftFrontDrive.setPower(leftfrontPower);
-            rightFrontDrive.setPower(rightfrontPower);
-            leftBackDrive.setPower(leftbackPower);
-            rightBackDrive.setPower(rightbackPower);
-
-            if (gamepad2.b) {
-                imu.resetYaw();
-            }
-
+        leftFrontDrive.setPower(leftfrontPower);
+        rightFrontDrive.setPower(rightfrontPower);
+        leftBackDrive.setPower(leftbackPower);
+        rightBackDrive.setPower(rightbackPower);
+            
+        if (gamepad2.b) {
+            imu.resetYaw();
         }
     }
 }
+
