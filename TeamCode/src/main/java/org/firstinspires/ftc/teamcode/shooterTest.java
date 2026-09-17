a3d6450a359468657eb5af58a0b98c6d2a2db31a
rujhaan-commits <rujhaansarup@gmail.com>
2026-09-17 13:29:41 +0100

    -Servo sync test -added shooter part to it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooterTest.java
@@ -1,29 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name = "Servotest", group = "TeleOp")
-public class ServoTest extends OpMode {
+@TeleOp(name = "shooterTest", group = "TeleOp")
+public class shooterTest extends OpMode {
     private Servo leftServo;
+    private DcMotor shooter;
     private Servo rightServo;
     @Override
     public void init(){
         rightServo = hardwareMap.get(Servo.class, "rightServo");
         leftServo = hardwareMap.get(Servo.class, "leftServo");
+        shooter = hardwareMap.get(DcMotor.class, "Shooter");
         rightServo.setDirection(Servo.Direction.REVERSE);
         leftServo.setPosition(0.5);
         rightServo.setPosition(0.5);
     }
     public void loop(){
-        if (gamepad1.a){
+        if (gamepad1.dpad_up){
             leftServo.setPosition(1.0);
             rightServo.setPosition(1.0);
-        if (gamepad1.b){
+        if (gamepad1.dpad_down){
             leftServo.setPosition(0.0);
             rightServo.setPosition(0.0);
+        if (gamepad1.a){
+            shooter.setPower(1.0);
+        }
+
         }
         }
         telemetry.addData("Left Servo", leftServo.getPosition());
